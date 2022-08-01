ff94cb7253ae6676ca5d63119d3e7a33eb6117eb
Constantin Lisnic <costelisnic98@gmail.com>
2022-08-01 18:44:26 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,85 @@
+const numbers = document.querySelectorAll(".number");
+const display = document.querySelector(".displayText");
+const operations = document.querySelectorAll(".operator");
+const equal = document.querySelector(".equal");
+const clear = document.querySelector(".clear");
+const minus = document.querySelector(".minus");
+const percent = document.querySelector(".percent");
+const dot = document.querySelector(".dot");
+
+let valueMemory = [];
+let operationMemory = [];
+let typeOnClean = true;
+
+function cleanMemory() {
+    valueMemory = [];
+    operationMemory = [];
+}
+
+numbers.forEach(number => number.addEventListener("click", e => {
+    if (typeOnClean) {
+        display.innerText = "";
+        typeOnClean = false;
+        display.append(e.target.innerText);
+    } else {
+        display.append(e.target.innerText);
+    }
+}))
+
+operations.forEach(operation => operation.addEventListener("click", e => {
+    if (operationMemory.length === 0) {
+        valueMemory.push(Number(display.innerText));
+        operationMemory.push((e.target.innerText));
+        typeOnClean = true;
+        e.target.classList.add("pressed");
+    }
+}))
+
+equal.addEventListener("click", () => {
+    if (valueMemory.length >= 1) {
+        let result;
+        valueMemory.push(Number(display.innerText))
+        switch (operationMemory[0]) {
+            case "+":
+                result = valueMemory[0] + valueMemory[1];
+                break;
+            case "-":
+                result = valueMemory[0] - valueMemory[1];
+                break;
+            case "×":
+                result = valueMemory[0] * valueMemory[1];
+                break;
+            case "÷":
+                result = valueMemory[0] / valueMemory[1];
+        }
+        cleanMemory()
+        display.innerText = "";
+        typeOnClean = true;
+        operations.forEach(operation => operation.classList.remove("pressed"));
+        display.append(result);
+    }
+})
+
+clear.addEventListener("click", () => {
+    display.innerText = "0";
+    cleanMemory();
+    operations.forEach(operation => operation.classList.remove("pressed"));
+    typeOnClean = true;
+})
+
+percent.addEventListener("click", () => {
+    const result = parseInt(display.innerText) / 100;
+    display.innerText = "";
+    display.append(result);
+    typeOnClean = true;
+})
+
+minus.addEventListener("click", () => {
+    const result = parseInt("-" + display.innerText);
+    display.innerText = "";
+    display.append(result);
+})
+
+dot.addEventListener("click", () => {
+    !display.innerText.includes(".") && display.append(".");
+})
